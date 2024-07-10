41067d45b2354ed0a923d850675e279dbd8409d9
Tsuyoshi Kamada <ms.2bpd.cch@gmail.com>
2024-07-11 01:30:48 +0900

    課題１５コミット

---
+++ yamaoKadai15/AddItemViewController.swift
@@ -0,0 +1,12 @@
+
+import UIKit
+
+class AddItemViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
